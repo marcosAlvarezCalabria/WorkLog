7574b9a8a55643ad99a79782b1e837150d1caca1
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-12-30 19:28:37 +0000

    app running and conected with database

--- api/configs/db.configs.js
+++ api/configs/db.configs.js
@@ -1,7 +1,7 @@
 
 const mongoose = require ('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'MONGODB://loclahost/27017/CBOOT';
+const MONGODB_URI = process.env.MONGODB_URI || 'MONGODB://localhost/27017/CBOOT';
 
 mongoose.connect(MONGODB_URI)
     .then(() => console.info(`Sucessfully connect to the database ${MONGODB_URI}`))
